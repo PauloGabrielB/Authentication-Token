1f63daabb898c231bdafee74bacbb0f3fdc5e6e4
Rafael Meneses <meneses.tio@gmail.com>
2023-07-05 17:18:09 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const products = [
+    {
+        name: 'Apple MacBook Pro 17"',
+        color: 'Prata',
+        amount: 1,
+        price: 2999,
+    },
+    {
+        name: 'Microsoft Surface Pro',
+        color: 'Branco',
+        amount: 5,
+        price: 1999,
+    },
+    {
+        name: 'Magic Mouse 2',
+        color: 'Preto',
+        amount: 2,
+        price: 99,
+    },
+    {
+        name: 'Google Pixel Phone',
+        color: 'Cinza',
+        amount: 2,
+        price: 799,
+    },
+    {
+        name: 'Apple Watch 5',
+        color: 'Vermelho',
+        amount: 3,
+        price: 999,
+    },
+];
