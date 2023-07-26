79dd346302c9b183d3235050587b5f81af0ef139
rtio <meneses.tio@gmail.com>
2023-07-26 20:28:14 -0300

    Começa de novo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const products = [
+const meusProdutos = [
     {
         name: 'Apple MacBook Pro 17"',
         color: 'Prata',
@@ -37,95 +37,20 @@ const products = [
     },
 ];
 
-function criarLinha() {
-    const linha = document.createElement('tr');
-    return linha;
-}
-
-function criarCelula(valor) {
-    const celula = document.createElement('td');
-    celula.innerHTML = valor;
-    return celula;
-}
-
-function criarCabecalho() {
-    const cabecalho = document.createElement('thead');
-    return cabecalho;
-}
-
-function criarCorpo() {
-    const corpo = document.createElement('tbody');
-    return corpo;
-}
-
-function criarCelulaCabecalho(valor) {
-    const cabecalho = document.createElement('th');
-    cabecalho.innerHTML = valor;
-    return cabecalho;
-}
-
-function criarRodape() {
-    const rodape = document.createElement('tfoot');
-    return rodape;
-}
-
-function criarTabela() {
-    const tabela = document.createElement('table');
-
-    const cabecalho = criarCabecalho();
-    const celulaCabecalhoNome = criarCelulaCabecalho('Nome do produto');
-    const celulaCabecalhoCor = criarCelulaCabecalho('Cor');
-    const celulaCabecalhoQuantidade = criarCelulaCabecalho('Quantidade');
-    const celulaCabecalhoPreco = criarCelulaCabecalho('Preço');
-    const linhaCabecalho = criarLinha();
-
-    linhaCabecalho.appendChild(celulaCabecalhoNome);
-    linhaCabecalho.appendChild(celulaCabecalhoCor);
-    linhaCabecalho.appendChild(celulaCabecalhoQuantidade);
-    linhaCabecalho.appendChild(celulaCabecalhoPreco);
-
-    cabecalho.appendChild(linhaCabecalho);
-    tabela.appendChild(cabecalho);
-
-    const corpo = criarCorpo();
-    products.forEach((produto) => {
-        const celulaNome = criarCelula(produto.name);
-        const celulaCor = criarCelula(produto.color);
-        const celulaQuantidade = criarCelula(produto.amount);
-        const celulaPreco = criarCelula(produto.price);
-
-        const linha = criarLinha();
-
-        linha.appendChild(celulaNome);
-        linha.appendChild(celulaCor);
-        linha.appendChild(celulaQuantidade);
-        linha.appendChild(celulaPreco);
-
-        corpo.appendChild(linha);
+function imprimirProdutos(products){
+    const table = document.querySelector('#tabela-produtos');
+    const tbody = document.createElement('tbody');
+    products.forEach((product) => {
+        const tr = document.createElement('tr');
+        for (const key in product) {
+            const tdName = document.createElement('td');
+            const valor = product[key];
+            tdName.textContent = valor;
+            tr.appendChild(tdName);
+        }   
+        tbody.appendChild(tr); 
     });
-    tabela.appendChild(corpo);
-
-    const rodape = criarRodape();
-    const celulaRodapeTotal = criarCelulaCabecalho('Total');
-    const celulaRodapeQuantidadeTotal = criarCelula('12121')
-    const celulaRodapePrecoTotal = criarCelula('12121')
-
-    const linhaRodape = criarLinha();
-
-    linhaRodape.appendChild(celulaRodapeTotal);
-    linhaRodape.appendChild(celulaRodapeQuantidadeTotal);
-    linhaRodape.appendChild(celulaRodapePrecoTotal);
-
-    rodape.appendChild(linhaRodape);
-    tabela.appendChild(rodape);
-
-    return tabela;
+    table.appendChild(tbody);
 }
 
-function renderizar() {
-    const tabela = criarTabela();
-    const containerPrincipal = document.getElementById('container');
-    containerPrincipal.appendChild(tabela);
-}
-
-renderizar();
+imprimirProdutos(meusProdutos);
