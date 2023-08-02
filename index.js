658acb73b5812ce6d698e23c48790e8532639830
rtio <meneses.tio@gmail.com>
2023-08-02 20:29:40 -0300

    Aula quarta

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
 const meusProdutos = [
     {
+        id: 1,
         name: 'Apple MacBook Pro 17"',
         color: 'Prata',
         amount: 1,
         price: 2999,
     },
     {
+        id: 2,
         name: 'Microsoft Surface Pro',
         color: 'Branco',
         amount: 5,
         price: 1999,
     },
     {
+        id: 3,
         name: 'Magic Mouse 2',
         color: 'Preto',
         amount: 2,
         price: 99,
     },
     {
+        id: 4,
         name: 'Google Pixel Phone',
         color: 'Cinza',
         amount: 2,
         price: 799,
     },
     {
+        id: 5,
         name: 'Apple Watch 5',
         color: 'Vermelho',
         amount: 3,
         price: 999,
     },
     {
+        id: 6,
         name: 'Apple iPad Pro 10.5"',
         color: 'Dourado',
         amount: 1,
@@ -37,34 +43,66 @@ const meusProdutos = [
     },
 ];
 
-const cabecalho = {
-    'col1': 'Nome do produto',
-    'col2': 'Cor',
-    'col3': 'Quantidade',
-    'col4': 'Preço',
+const cabecalho = [
+    'id',
+    'Nome do produto',
+    'Cor',
+    'Quantidade',
+    'Preço',
+    'Ações',
+];
+
+function criarCabecalho(){
+    const thead = document.createElement('thead');
+    thead.classList.add('text-xs', 'text-gray-700', 'uppercase', 'bg-gray-50', 'dark:bg-gray-700', 'dark:text-gray-400');
+    
+    const trHead = document.createElement('tr');
+
+    cabecalho.forEach((nomeCabecalho) => {
+        const th = document.createElement('th');
+        th.setAttribute('scope', 'col');
+        th.classList.add('px-6', 'py-3');
+        th.textContent = nomeCabecalho;
+        trHead.appendChild(th);
+    });
+
+    thead.appendChild(trHead);
+    return thead;
 };
+
+function criarRodape(produtos){
+    const tfoot = document.createElement('tfoot');
+    const trRodape = document.createElement('tr');
+    trRodape.classList.add('font-semibold', 'text-gray-900', 'dark:text-white');
+
+    const thTotal = document.createElement('th');
+    thTotal.setAttribute('colspan', '3');
+    thTotal.setAttribute('scope', 'row');
+    thTotal.classList.add('px-6', 'py-3', 'text-base');
+    thTotal.textContent = 'Total';
+    trRodape.appendChild(thTotal);
+
+    const tdQuantidade = document.createElement('td');
+    tdQuantidade.classList.add('px-6', 'py-3');
+    tdQuantidade.textContent = produtos.reduce((acumulador, produto) => acumulador + produto.amount, 0 );
+    trRodape.appendChild(tdQuantidade);
+
+    const tdPrecoTotal = document.createElement('td');
+    tdPrecoTotal.classList.add('px-6', 'py-3');
+    const somaTotal = produtos.reduce((acumulador, produto) => acumulador + produto.price, 0 );
+    tdPrecoTotal.textContent = somaTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    trRodape.appendChild(tdPrecoTotal);
+
+    tfoot.appendChild(trRodape);
+    return tfoot;
+}
 
 function imprimirProdutos(products){
     const table = document.querySelector('#tabela-produtos');
     table.innerHTML = '';
     table.classList.add('w-full', 'text-sm', 'text-left', 'text-gray-500', 'dark:text-gray-400');
 
-    const thead = document.createElement('thead');
-    thead.classList.add('text-xs', 'text-gray-700', 'uppercase', 'bg-gray-50', 'dark:bg-gray-700', 'dark:text-gray-400');
-    
-    const trHead = document.createElement('tr');
-
-    for (const chave in cabecalho) {
-        const nomeCabecalho = cabecalho[chave];
-        const th = document.createElement('th');
-        th.setAttribute('scope', 'col');
-        th.classList.add('px-6', 'py-3');
-        th.textContent = nomeCabecalho;
-        trHead.appendChild(th);
-    }
-
-    thead.appendChild(trHead);
-
+    const thead = criarCabecalho();
     table.appendChild(thead);
 
     const tbody = document.createElement('tbody');
@@ -76,7 +114,7 @@ function imprimirProdutos(products){
             coluna.classList.add('px-6', 'py-4');
             if (key === 'name') {
                 coluna = document.createElement('th');
-                coluna.classList.add('font-medium', 'text-gray-900', 'whitespace-nowrap', 'dark:text-white');
+                coluna.classList.add('px-6', 'py-4', 'font-medium', 'text-gray-900', 'whitespace-nowrap', 'dark:text-white');
                 coluna.setAttribute('scope', 'row');
             }
             const valor = product[key];
@@ -85,33 +123,29 @@ function imprimirProdutos(products){
                 coluna.textContent = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
             }
             tr.appendChild(coluna);
-        }   
+        }
+        
+        const tdAcoes = document.createElement('td');
+        tdAcoes.classList.add('flex', 'items-center', 'px-6', 'py-4', 'space-x-3');
+        
+        const linkEditar = document.createElement('a');
+        linkEditar.setAttribute('href', '#');
+        linkEditar.classList.add('font-medium', 'text-red-600', 'dark:text-red-500', 'hover:underline');
+        linkEditar.textContent = 'Excluir';
+        linkEditar.addEventListener('click', (event) => {
+            event.preventDefault();
+            const novaLista = excluirProduto(product.id);
+            imprimirProdutos(novaLista);
+        });
+
+        tdAcoes.appendChild(linkEditar);
+
+        tr.appendChild(tdAcoes);
+         
         tbody.appendChild(tr); 
     });
 
-    const tfoot = document.createElement('tfoot');
-    const trRodape = document.createElement('tr');
-    trRodape.classList.add('font-semibold', 'text-gray-900', 'dark:text-white');
-
-    const thTotal = document.createElement('th');
-    thTotal.setAttribute('colspan', '2');
-    thTotal.setAttribute('scope', 'row');
-    thTotal.classList.add('px-6', 'py-3', 'text-base');
-    thTotal.textContent = 'Total';
-    trRodape.appendChild(thTotal);
-    
-    const tdQuantidade = document.createElement('td');
-    tdQuantidade.classList.add('px-6', 'py-3');
-    tdQuantidade.textContent = products.reduce((acumulador, produto) => acumulador + produto.amount, 0 );
-    trRodape.appendChild(tdQuantidade);
-
-    const tdPrecoTotal = document.createElement('td');
-    tdPrecoTotal.classList.add('px-6', 'py-3');
-    const somaTotal = products.reduce((acumulador, produto) => acumulador + produto.price, 0 );
-    tdPrecoTotal.textContent = somaTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    trRodape.appendChild(tdPrecoTotal);
-
-    tfoot.appendChild(trRodape);
+    const tfoot = criarRodape(products);
 
     table.appendChild(tfoot);
 
@@ -125,21 +159,36 @@ function obterFormData(){
     const inputCor = document.querySelector('#color');
     const inputQuantidade = document.querySelector('#amount');
     const inputPreco = document.querySelector('#price');
-    const meuObj = {
+
+    const quantidade = Number(inputQuantidade.value);
+    const preco = Number(inputPreco.value);
+
+    return {
+        id: null,
         name: inputNome.value,
         color: inputCor.value,
-        amount: inputQuantidade.value,
-        price: inputPreco.value,
+        amount: quantidade,
+        price: preco,
     };
-    return meuObj;
 };
 
 function adicionarProduto(event){
     event.preventDefault();
     const produto = obterFormData();
+    produto.id = meusProdutos.length + 1;
     meusProdutos.push(produto);
     imprimirProdutos(meusProdutos);
 }
 
-const form = document.querySelector('#form-produto');
-form.addEventListener('submit', adicionarProduto);
+function excluirProduto(id){
+    const produtoIndice = meusProdutos.findIndex((produto) => produto.id === id);
+    meusProdutos.splice(produtoIndice, 1);
+    return meusProdutos;
+}
+
+function registrarListeners(){
+    const form = document.querySelector('#form-produto');
+    form.addEventListener('submit', adicionarProduto);
+}
+
+registrarListeners();
